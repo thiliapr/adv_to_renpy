c2a92f519883884ce5c55b984e74786be3dc214b
thiliapr <140580293+thiliapr@users.noreply.github.com>
2026-08-09 11:27:14 +0800

    脚本开始块后换行

--- convert_script.py
+++ convert_script.py
@@ -199,7 +199,7 @@ def convert_ast_to_renpy(current_script: str, article: SentenceArticle) -> str:
     new_article = [(pointer, [sentence.compile(environment) for sentence in sentences]) for pointer, sentences in new_article]
 
     # 写成 Ren'Py 脚本
-    script = f"label {current_script}__start:\n    jump {current_script}__pointer_{article[0][0]}\n"
+    script = f"label {current_script}__start:\n    jump {current_script}__pointer_{article[0][0]}\n\n"
     for pointer, sentences in new_article:
         script += f"label {current_script}__pointer_{pointer}:\n"
         for sentence in sentences:
@@ -232,7 +232,7 @@ def main(args: argparse.Namespace):
 
     # 初始化游戏脚本
     renpy_script_path = args.output_script_path or (args.project_dir / "game/script.rpy")
-    renpy_script_path.write_text(f"define config.old_substitutions = False\ndefine config.safe_text = True\n\nlabel start:\n    jump {args.entry_script}__start", encoding="utf-8")
+    renpy_script_path.write_text(f"define config.old_substitutions = False\ndefine config.safe_text = True\n\nlabel start:\n    jump {args.entry_script}__start\n\n", encoding="utf-8")
 
     # 从入口脚本开始读取脚本
     scripts_to_convert: set[str] = {args.entry_script}
